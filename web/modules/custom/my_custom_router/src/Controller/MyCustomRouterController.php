2367e6501faa0fcbd85828f082c4138c77c11dc6
Maneesha Binish <maneeshabinish@gmail.com>
2023-10-18 18:45:59 +0530

    Added a Twig Template to the Page Controller

--- web/modules/custom/my_custom_router/src/Controller/MyCustomRouterController.php
+++ web/modules/custom/my_custom_router/src/Controller/MyCustomRouterController.php
@@ -4,19 +4,25 @@ namespace Drupal\my_custom_router\Controller;
 use Drupal\Core\Controller\ControllerBase;
 
 class MyCustomRouterController extends ControllerBase {
-    public function mytestpage(){
+  public function test_page(){
+ 
+    return [
+      
+      '#theme' => 'test_page',
+      '#text' => 'Hello, world',
 
-        return [
-            '#markup' => 'Hello, world',
-          ];
-    }
+    ];
+  }
 
-    public function arguments($number, $num2){
+  public function arguments_display($first_arg, $second_arg){
+    
 
-       return [
-        
-            '#markup' =>  $this->t('Hi, this is a random number: @arg1 @arg2', ['@arg1' => $number,'@arg2'=> $num2]),
-        
-          ];
-    }
+    return [
+      
+      '#theme' => 'arguments_display',
+      '#num1' => $first_arg,
+      '#num2'=> $second_arg,
+      
+    ];
+  }
 }
